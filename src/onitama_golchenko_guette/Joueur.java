67ef32e7e0c359879f21208c7443d558ed639595
dagou <dagou@DESKTOP-3NISKNR>
2020-11-24 17:00:12 +0100

    A toi de commenter amigo

---
+++ src/onitama_golchenko_guette/Joueur.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package onitama_golchenko_guette;
+
+/**
+ *
+ * @author dagou
+ */
+public class Joueur {
+    String Nom;
+    String Couleur;
+    Jeton [] ListeJetons = new Jeton [21];
+}
